3d0872ef1b3c2b05b537b305a18a9402122955b0
maximekgn <kogonmaxime@gmail.com>
2024-08-10 20:31:15 +0000

    #

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,38 +1,42 @@
-import image from "../../assets/fruits-splash.png"
-import aos from "aos"
-import "aos/dist/aos.css"
-import { useEffect } from "react"
+import image from "../../assets/fruits-splash.png";
+import aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+
 const Banner = () => {
   useEffect(() => {
-    aos.init()
-  })
+    aos.init();
+  }, []);
+
   return (
-    <section className="bg-secondary/10">
-      <div className="container flex justify-center items-center space-y-6 sm:space-y-0 py-14 ">
-        {/* Banner image */} 
+    <section className="bg-secondary/10 py-14">
+      <div className="container mx-auto flex flex-col md:flex-row justify-center items-center space-y-6 md:space-y-0 md:space-x-6">
+        {/* Banner image */}
         <img
           data-aos="fade-up"
           data-aos-duration="3000"
-          src={image} alt="" className="w-[300px] md:max-w[400px] h-full object-cover" />
+          src={image}
+          alt="Fruits Splash"
+          className="w-full max-w-xs md:max-w-md h-auto object-cover"
+        />
         {/* Brand info */}
-        <div className="flex flex-col justify-center text-center md:text-left">
+        <div className="flex flex-col justify-center text-center md:text-left px-4">
           <div data-aos="fade-up" data-aos-duration="2500">
-            <h1 className="text-3xl font-bold">brand INFO</h1>
-            <p>
+            <h1 className="text-3xl font-bold">Brand INFO</h1>
+            <p className="mt-4">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. At inventore libero voluptatem laborum? Et vel pariatur magnam dignissimos maxime nisi, magni, a sint suscipit ipsam quidem impedit laudantium ullam explicabo.
             </p>
-            <p>
+            <p className="mt-2">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo, qui praesentium eos eaque magnam nihil, commodi magni quos facilis atque, placeat non voluptates laborum quasi? Soluta ut ipsum mollitia veritatis.
-
             </p>
             <button className="primary-btn flex items-center gap-1 mt-5">
-                Learn More
+              Learn More
             </button>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
--- src/components/Banner/Banner3.tsx
+++ src/components/Banner/Banner3.tsx
@@ -1,44 +1,46 @@
-import image from "../../assets/banner-bg.jpg"
-import aos from "aos"
-import "aos/dist/aos.css"
-import { useEffect } from "react"
+import image from "../../assets/banner-bg.jpg";
+import aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const bgStyle = {
-    backgroundImage: `url(${image})`,
-    backgroudPosition: "center",
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-}
+  backgroundImage: `url(${image})`,
+  backgroundPosition: "center",
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+};
+
 const Banner3 = () => {
   useEffect(() => {
-    aos.init()
-  })
+    aos.init();
+  }, []);
+
   return (
-    <section data-aos="zoom-in-left" data-aos-duration="3000" >
-      <div 
-        style={bgStyle} className=" rounded-lg container flex justify-center items-center space-y-6 sm:space-y-0 py-14 ">
-        {/* Blank div */} 
-        <div></div>
+    <section data-aos="zoom-in-left" data-aos-duration="3000">
+      <div
+        style={bgStyle}
+        className="rounded-lg container mx-auto flex flex-col md:flex-row justify-center items-center text-white py-14 space-y-6 md:space-y-0 md:space-x-6"
+      >
+        {/* Blank div for spacing */}
+        <div className="hidden md:block w-1/4"></div>
         {/* Brand info */}
-        <div className="flex flex-col justify-center text-center md:text-left">
+        <div className="flex flex-col justify-center text-center md:text-left px-4 md:w-3/4">
           <div data-aos="fade-up" data-aos-duration="2500">
-            <h1 className="text-3xl font-bold">brand INFO</h1>
-            <p>
+            <h1 className="text-3xl font-bold">Brand INFO</h1>
+            <p className="mt-4">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. At inventore libero voluptatem laborum? Et vel pariatur magnam dignissimos maxime nisi, magni, a sint suscipit ipsam quidem impedit laudantium ullam explicabo.
             </p>
-            <p>
+            <p className="mt-2">
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo, qui praesentium eos eaque magnam nihil, commodi magni quos facilis atque, placeat non voluptates laborum quasi? Soluta ut ipsum mollitia veritatis.
-
             </p>
             <button className="primary-btn flex items-center gap-1 mt-5">
-                Learn More
+              Learn More
             </button>
           </div>
         </div>
-        
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Banner3
+export default Banner3;
